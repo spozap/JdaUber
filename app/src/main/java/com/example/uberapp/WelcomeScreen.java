4cc1e65b417e7ff149cf24f0c6aaaf69d1cb90ae
spozap <sergi.poza@iesjoandaustria.org>
2019-10-15 16:43:38 +0200

    Empezando modelaje de usuarios

---
+++ app/src/main/java/com/example/uberapp/WelcomeScreen.java
@@ -0,0 +1,37 @@
+package com.example.uberapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomeScreen extends AppCompatActivity {
+
+    private Button btnUser;
+    private Button btnDriver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_screen);
+
+        btnUser = findViewById(R.id.btnUser);
+        btnDriver = findViewById(R.id.btnDriver);
+
+        btnUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              Intent intent = new Intent(v.getContext(), Login.class);
+              startActivity(intent);
+            }
+        });
+       /*
+        btnDriver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(this,)
+            }
+        }); */
+    }
+}
