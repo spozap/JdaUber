5bc9f00df5ee96df3928533fcc55ddb5dc6eee58
spozap <sergi.poza@iesjoandaustria.org>
2020-02-18 18:46:40 +0100

    image upload to firebase storage

--- app/src/main/java/com/example/uberapp/profileFragment.java
+++ app/src/main/java/com/example/uberapp/profileFragment.java
@@ -16,10 +16,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Objects;
+import androidx.lifecycle.ViewModelProviders;
 
 import static android.app.Activity.RESULT_OK;
 
@@ -34,6 +31,7 @@ public class profileFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
+        profileViewModel = ViewModelProviders.of(this).get(profileViewModel.class);
 
         usernameTxt = root.findViewById(R.id.usernameTxt);
         passwordTxt = root.findViewById(R.id.passwordTxt);
@@ -42,7 +40,7 @@ public class profileFragment extends Fragment {
         userProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                Intent i = new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 startActivityForResult(i,20);
             }
         });
@@ -55,11 +53,12 @@ public class profileFragment extends Fragment {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == 20 && resultCode == RESULT_OK && data != null){
-
-            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-
-            if (bitmap != null){
-                userProfile.setImageBitmap(Bitmap.createScaledBitmap(bitmap,599 ,599,false));
+            Bitmap bitmap = null;
+            Uri uri;
+            uri = data.getData();
+            if (uri != null){
+                //userProfile.setImageBitmap(Bitmap.createScaledBitmap(bitmap,599 ,599,false));
+                profileViewModel.uploadImage(uri);
             }
         }
     }
--- app/src/main/java/com/example/uberapp/profileViewModel.java
+++ app/src/main/java/com/example/uberapp/profileViewModel.java
@@ -1,11 +1,48 @@
 package com.example.uberapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModel;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class profileViewModel extends ViewModel {
 
+    private FirebaseAuth mAuth;
+
+    public void uploadImage(Uri uri){
+
+        mAuth = FirebaseAuth.getInstance();
+
+        if (uri != null){
+            //Creating a child with the username's UID
+            StorageReference mImageRef = FirebaseStorage.getInstance().getReference().child(mAuth.getUid());
+            mImageRef.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Log.d("Storage","Img subida");
+                }
+            })
+            .addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.d("Storage","Error al subir la imagen");
+                }
+            });
+
+        }
+
+    }
 
 }
